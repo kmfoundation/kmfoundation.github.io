18212b9ddec8d80334565046dd0dfb1ea761addf
kmfdev <kochimusicfoundation@gmail.com>
2020-02-18 16:41:57 +0530

    initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+website
+=======
+Karuna website
---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    var i18n = $.i18n();
+
+    function loadpage() {
+        var browserLanguage = navigator.language.split('-')[0];
+        i18n.locale = $(this).data('language') || browserLanguage || "en";
+        i18n.load('i18n/kmf-' + i18n.locale + '.json', i18n.locale).done(() => {
+            $("#homepage").i18n();
+        });
+    }
+    loadpage();
+    $('.language').on('click', loadpage);
+});
